cbcf242fbc6eecbe70d43800500848a18bf704f7
ashikmahmud <ashikmamud187@gmail.com>
2022-03-21 02:44:20 +0600

    yeah created another section called best apartments section

---
+++ src/assets/image.js
@@ -0,0 +1,6 @@
+import apt1 from "./apt1.jpeg";
+import apt2 from "./apt2.jpeg";
+import apt3 from "./apt3.jpeg";
+
+export { apt1, apt3, apt2 };
+
--- src/components/Best/Best.jsx
+++ src/components/Best/Best.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { apt1, apt2, apt3 } from "../../assets/image";
 import "./Best.css";
 const Best = () => {
   return (
     <>
       <section id="best">
-        <div className="container">
-          <h2>From best</h2>
+        <div className="best-container container">
+          <h2>Find best rated properties </h2>
+          <div className="best-content">
+            <ul className="tab-buttons">
+              <li className="active">All</li>
+              <li>Commercial</li>
+              <li>Residential</li>
+              <li>Agriculture</li>
+            </ul>
+            <div className="best-grid">
+              <div className="item">
+                <img src={apt1} alt={apt1} />
+              </div>
+              <div className="item">
+                <img src={apt2} alt={apt1} />
+              </div>
+              <div className="item">
+                <img src={apt3} alt={apt1} />
+              </div>
+            </div>
+            <div className="center-text ">
+              <button className="btn">Show more</button>
+            </div>
+          </div>
         </div>
       </section>
     </>
