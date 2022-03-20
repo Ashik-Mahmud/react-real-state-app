3afb0ecc4a9c04794278cd2a529771ef87fbcc28
ashikmahmud <ashikmamud187@gmail.com>
2022-03-21 03:37:09 +0600

    added new section called featured section

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,6 +1,16 @@
 import apt1 from "./apt1.jpeg";
 import apt2 from "./apt2.jpeg";
 import apt3 from "./apt3.jpeg";
+import bath1 from "./bath1.jpg";
+import bath2 from "./bath2.jpg";
+import bed1 from "./bed1.jpg";
+import bed2 from "./bed2.jpg";
+import bed3 from "./bed3.jpg";
+import bed4 from "./bed4.jpg";
+import house1 from "./house1.jpg";
+import house2 from "./house2.jpg";
+import kitchen from "./kitchen.jpg";
+import livingRoom from "./living-room.jpg";
 
-export { apt1, apt3, apt2 };
+export { apt1, apt3, apt2, bath1, bath2, bed1, bed2, bed3, bed4, house1, house2, kitchen, livingRoom };
 
--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,11 +1,145 @@
 import React from "react";
+import {
+  bath1,
+  bath2,
+  bed1,
+  bed2,
+  bed3,
+  bed4,
+  house1,
+  house2,
+  kitchen,
+  livingRoom,
+} from "../../assets/image";
 import "./Featured.css";
 const Featured = () => {
   return (
     <>
       <section id="featured">
-        <div className="container">
-          <h2>From Featured</h2>
+        <div className="featured-container container">
+          <div className="section-title">
+            <h2>Top Featured listing</h2>
+            <p>Select listing by State, Address, on your views</p>
+          </div>
+          <div className="featured-grid">
+            <div className="item house">
+              <img src={house1} alt={house1} />
+            </div>
+            <div className="item">
+              <img src={bath1} alt={bath1} />
+            </div>
+            <div className="item">
+              <img src={bed1} alt={bed1} />
+            </div>
+            <div className="item">
+              <img src={bed2} alt={bed2} />
+            </div>
+            <div className="item">
+              <img src={bath2} alt={bath2} />
+            </div>
+            <div className="item address">
+              <div>
+                <h3>123 Acme St. Dallas TX</h3>
+                <ul>
+                  <li>
+                    <b>Bedrooms</b>
+                    <span>5</span>
+                  </li>
+                  <li>
+                    <b>Bathrooms</b>
+                    <span>7</span>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <b>House for sale</b>
+                    <h2>$ 21,5445</h2>
+                  </li>
+                  <li>
+                    <b>Square feet</b>
+                    <span>8120</span>
+                  </li>
+                  <li>
+                    <b>Est Payment</b>
+                    <span>$14000/month</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="item details">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                deleniti at iusto aperiam vero aliquid doloribus ipsam non
+                sapiente eum neque obcaecati, laborum rem laboriosam modi quidem
+                enim, odio eius!
+              </p>
+              <button className="btn">View Listing</button>
+            </div>
+          </div>
+
+          {/* second  */}
+
+          <div className="featured-grid">
+            <div className="item">
+              <img src={kitchen} alt={kitchen} />
+            </div>
+
+            <div className="item">
+              <img src={bed4} alt={bed4} />
+            </div>
+            <div className="item house">
+              <img src={house2} alt={house2} />
+            </div>
+            <div className="item">
+              <img src={bed3} alt={bed2} />
+            </div>
+
+            <div className="item">
+              <img src={livingRoom} alt={livingRoom} />
+            </div>
+            <div className="item details">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                deleniti at iusto aperiam vero aliquid doloribus ipsam non
+                sapiente eum neque obcaecati, laborum rem laboriosam modi quidem
+                enim, odio eius!
+              </p>
+              <button className="btn">View Listing</button>
+            </div>
+            <div className="item address">
+              <div>
+                <h3>123 Acme St. Dallas TX</h3>
+                <ul>
+                  <li>
+                    <b>Bedrooms</b>
+                    <span>5</span>
+                  </li>
+                  <li>
+                    <b>Bathrooms</b>
+                    <span>7</span>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <b>House for sale</b>
+                    <h2>$ 21,5445</h2>
+                  </li>
+                  <li>
+                    <b>Square feet</b>
+                    <span>8120</span>
+                  </li>
+                  <li>
+                    <b>Est Payment</b>
+                    <span>$14000/month</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
