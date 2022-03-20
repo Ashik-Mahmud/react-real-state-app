2dabc83c7f292d2affff67b3582becd6b1820d53
ashikmahmud <ashikmamud187@gmail.com>
2022-03-21 02:16:42 +0600

    yeah add another section called hero banner

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { RiSearch2Line } from "react-icons/ri";
 import "./Hero.css";
 const Hero = () => {
   return (
     <>
       <section id="hero">
-        <div className="hero container">
-          <h2>From Hero</h2>
+        <div className="hero-container">
+          <h1>Find the Perfect place</h1>
+          <p>
+            Follow along as I build this mock real estate website from scratch
+            using React JS. I will show you how to use the useState hook to
+            toggle our mobile drop down menu as well{" "}
+          </p>
+          <div className="input-groups">
+            <input
+              type="search"
+              id="search"
+              placeholder="Search Luxury Apartments"
+            />
+            <div className="search-buttons">
+              <div>
+                <input name="apartments" type="radio" id="buy" />
+                <label htmlFor="buy">Buy</label>
+              </div>
+              <div>
+                <input name="apartments" type="radio" id="rent" />
+                <label htmlFor="rent">Rent</label>
+              </div>
+              <button className="btn">
+                <RiSearch2Line />
+              </button>
+            </div>
+          </div>
         </div>
       </section>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BsGrid } from "react-icons/bs";
 import { FaRegWindowClose } from "react-icons/fa";
+import { SiHomeadvisor } from "react-icons/si";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -10,14 +11,23 @@ const Navbar = () => {
       <header id="header" onClick={handleTitle}>
         <div className="container navbar">
           <nav>
-            <a href="#home">
-              Real<span> State</span>
+            <a href="#home" className="d-flex">
+              <span className="d-flex">
+                <SiHomeadvisor /> Real{" "}
+              </span>
+              State
             </a>
             <button className="btn">Sign Up</button>
             <ul className={`primary-menu ${show ? "active" : ""}`}>
-              {["home", "best", "featured", "about", "contact"].map((link) => (
-                <NavLinks pathName={link} linkName={link} />
-              ))}
+              {["home", "best", "featured", "about", "contact"].map(
+                (link, index) => (
+                  <NavLinks
+                    key={link + index}
+                    pathName={link}
+                    linkName={link}
+                  />
+                )
+              )}
             </ul>
             <div className="menuIcon" onClick={() => setShow((prev) => !prev)}>
               {show ? <FaRegWindowClose /> : <BsGrid />}
@@ -41,6 +51,6 @@ const handleTitle = (e) => {
 };
 
 const changeTitle = (appTitle) => {
-  document.title = appTitle ? appTitle : "Real State" + " -RealState App";
+  document.title = appTitle ? appTitle : "Real State -RealState App";
 };
 export default Navbar;
